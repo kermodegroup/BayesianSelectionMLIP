c59c384c068d2806f194d70efa26f9719114b755
Thomas Rocke <45517493+thomas-rocke@users.noreply.github.com>
2025-02-28 13:47:18 +0000

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BayesianSelection',
-    version='0.1.0',    
+    version='1.0.0',    
     description='MLIP Dataset Bayesian selection',
     author='Thomas Rocke',
     author_email='thomas.rocke@warwick.ac.uk',
